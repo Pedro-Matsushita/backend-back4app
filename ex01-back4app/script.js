24b28adf0cda5a54983cecc1ea1ff43da37c02eb
Pedro Matsushita <137213776+Pedro-Matsushita@users.noreply.github.com>
2024-05-13 13:51:33 +0000

    ta

--- ex01-back4app/script.js
+++ ex01-back4app/script.js
@@ -6,8 +6,8 @@ const cbNaoConcluidas = document.getElementById("cbNaoConcluidas");
 
 const tarefaURL = "https://parseapi.back4app.com/classes/Tarefa";
 const headers = {
-  "X-Parse-Application-Id": "1cJocS0NTUEkciXEyZuWApiy4YcAzkICO0HElEnW",
-  "X-Parse-REST-API-Key": "7dl9VG5489Lu5nXteS9mqclTHaSbin5I0yVIPC5n",
+  "X-Parse-Application-Id": "F0tJ7ImU1PrIAShCSiSJRHpbfsULHJunrL7arE4E",
+  "X-Parse-REST-API-Key": "dys4kwriX5tZIP8OpZwNCkTnCWQjzzFoAqd4InLy",
 };
 const headersJson = {
   ...headers,
